ae406f8a95acac77c6b69c3cff042478074c47fd
SerafimPoch <serpochapinsky@gmail.com>
2018-08-13 14:18:07 +0300

    connect redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import registerServiceWorker from "./registerServiceWorker";
+import reducer from "./store/reducers";
+import App from "./containers/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
